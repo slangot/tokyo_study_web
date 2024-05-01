ad9978e45ad7a0064af13526edf8a84cfbc19af2
Shawn <shawn17@live.fr>
2024-05-01 17:33:43 +0200

    💄 add custom colors; add custom classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary": "#653C87",
+        "secondary": "#520380",
+        "third": "#3A025B",
+        "fourth": "#220135",
+        "medium-dark": "#1A0129",
+        "dark": "#11001C",
+        "dark-gray": 'rgb(17,24,39)',
+        "light": "#E6D7FA"
+      }
     },
   },
   plugins: [],
