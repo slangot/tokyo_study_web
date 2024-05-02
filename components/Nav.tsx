b69b2ec7ddb383082f62ae86d1ebe4b929f83e63
Shawn <shawn17@live.fr>
2024-05-02 17:50:10 +0200

    💄 ✨ add mobile and desktop responsive navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,29 +1,73 @@
 "use client"
 
-// next
+// React
+import { useState } from "react"
+
+// Next
 import Image from "next/image"
 import Link from "next/link"
 
+// Icons
+import { FaBars, FaMagnifyingGlass, FaRegCircleXmark } from "react-icons/fa6"
+
 const Nav = () => {
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   return (
-    <nav className="flex flex-between items-center w-full mb-4 pt-3">
-      <Link href="/" className="flex gap-2 flex-center">
+    <nav className="flex flex-between items-center w-full mb-4 mt-2 ">
+      <Link href="/" className="flex flex-1 flex-center">
         <Image src="/logo.png" alt="Tokyo Study logo" width={80} height={80} className="object-contain" />
       </Link>
-      <div className="flex flex-1 justify-evenly">
-        <Link href="/kana" className="">
-          Kana
-        </Link>
-        <Link href="/kanji" className="">
-          Kanji
-        </Link>
-        <Link href="/exercices" className="">
-          Exercices
-        </Link>
-        <Link href="/dictionary" className="">
-          Dictionnaire
-        </Link>
+
+      {/* Desktop Nav */}
+      <div className="sm:flex flex-1 items-center justify-center hidden">
+        <div className="flex flex-1 justify-evenly">
+          <Link href="/kana" className="">
+            Kana
+          </Link>
+          <Link href="/kanji" className="">
+            Kanji
+          </Link>
+          <Link href="/exercices" className="">
+            Exercices
+          </Link>
+          <Link href="/dictionary" className="flex items-center justify-center">
+            <FaMagnifyingGlass />
+          </Link>
+        </div>
+      </div>
+
+      {/* Mobile Nav */}
+      <div className="sm:hidden flex">
+        {toggleDropdown ? (
+          <div className='dropdown'>
+            <button className='dropdown_close' onClick={() => setToggleDropdown(false)}><FaRegCircleXmark /></button>
+            <Link href='/kana'
+              className='dropdown_link'
+              onClick={() => setToggleDropdown(false)}>
+              Kana
+            </Link>
+            <Link href='/kanji'
+              className='dropdown_link'
+              onClick={() => setToggleDropdown(false)}>
+              Kanji
+            </Link>
+            <Link href='/exercices'
+              className='dropdown_link'
+              onClick={() => setToggleDropdown(false)}>
+              Exercices
+            </Link>
+            <Link href='/dictionnary'
+              className='dropdown_link'
+              onClick={() => setToggleDropdown(false)}>
+              Dictionnaire
+            </Link>
+          </div>)
+          :
+          <div className="flex items-end justify-center mr-3" onClick={() => setToggleDropdown(true)}>
+            <FaBars size={25} />
+          </div>
+        }
       </div>
     </nav>
   )
